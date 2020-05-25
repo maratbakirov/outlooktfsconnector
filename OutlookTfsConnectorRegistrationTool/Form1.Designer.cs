e0d886b26df756745ec95a523cf155649da553fd
Marat Bakirov <marat.bakirov@insight.com>
2020-05-26 00:15:02 +0800

    #16 mini installer

---
+++ OutlookTfsConnectorRegistrationTool/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace OutlookTfsConnectorRegistrationTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btRegister = new System.Windows.Forms.Button();
+            this.btUnregister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btRegister
+            // 
+            this.btRegister.Location = new System.Drawing.Point(35, 79);
+            this.btRegister.Name = "btRegister";
+            this.btRegister.Size = new System.Drawing.Size(166, 58);
+            this.btRegister.TabIndex = 0;
+            this.btRegister.Text = "Register";
+            this.btRegister.UseVisualStyleBackColor = true;
+            this.btRegister.Click += new System.EventHandler(this.btRegister_Click);
+            // 
+            // btUnregister
+            // 
+            this.btUnregister.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btUnregister.Location = new System.Drawing.Point(256, 79);
+            this.btUnregister.Name = "btUnregister";
+            this.btUnregister.Size = new System.Drawing.Size(166, 58);
+            this.btUnregister.TabIndex = 1;
+            this.btUnregister.Text = "Unregister";
+            this.btUnregister.UseVisualStyleBackColor = true;
+            this.btUnregister.Click += new System.EventHandler(this.btUnregister_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(35, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Outlook TFS Connector Registration Tool";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 196);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btUnregister);
+            this.Controls.Add(this.btRegister);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btRegister;
+        private System.Windows.Forms.Button btUnregister;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OutlookTfsConnectorRegistrationTool/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OutlookTfsConnectorRegistrationTool
+{
+    public partial class Form1 : Form
+    {
+        private const string RegistryAddinPath = @"Software\Microsoft\Office\Outlook\Addins\OutlookTfsConnector";
+        //private const string RegistryDataPath = @"Software\Microsoft\Office\Outlook\AddinsData\OutlookTfsConnector";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btRegister_Click(object sender, EventArgs e)
+        {
+            //var path = "file:\\"+Path.GetDirectoryName(Application.StartupPath) + "\\OutlookTfsConnector.vsto|vstolocal";
+            //path = path.Replace('\\', '/');
+
+            var openFileDialog = new OpenFileDialog()
+            {
+
+                Filter = "VSTO extension (*.vsto)|*.vsto",
+                CheckFileExists = true,
+                CheckPathExists = true,
+                DefaultExt = "vsto",
+                InitialDirectory = Path.GetDirectoryName(Application.StartupPath),
+                RestoreDirectory = true
+            };
+            if (openFileDialog.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+
+            var path = "file:///" + openFileDialog.FileName + "|vstolocal";
+            path = path.Replace('\\', '/');
+
+
+            //Registry.CurrentUser.CreateSubKey(RegistryDataPath);
+            var key = Registry.CurrentUser.CreateSubKey(RegistryAddinPath);
+            key.SetValue("LoadBehavior",3);
+            key.SetValue("Description", "OutlookTfsConnector");
+            key.SetValue("FriendlyName", "OutlookTfsConnector");
+            key.SetValue("Manifest", path);
+            MessageBox.Show("Successfully registered the connector");
+        }
+
+        private void btUnregister_Click(object sender, EventArgs e)
+        {
+            Registry.CurrentUser.DeleteSubKeyTree(RegistryAddinPath);
+            MessageBox.Show("Successfully deregistered the connector");
+        }
+    }
+}
