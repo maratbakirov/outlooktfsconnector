16eb5396b8811e169c26d53cd52c92bf5c6470fb
Marat Bakirov <mbakirov@hotmail.com>
2020-05-28 23:52:37 +0800

    trying to fix certificate again

--- OutlookTfsConnector/certificate/VSinstallcertificate.cmd
+++ OutlookTfsConnector/certificate/VSinstallcertificate.cmd
@@ -1,3 +1,11 @@
 echo enter P@ssw0rd when prompted
+Rem the key seems to be rather random....depending on machine?
+Rem the key seems to be rather random....depending on machine?
+sn -d  VS_KEY_C1D3ACB8FBF1AGK4
+sn -d  VS_KEY_BD1B1F1C70B95C17
+sn -d  VS_KEY_0735BCA139159F1C 
+
+
 sn -i OutlookTfsConnector.pfx VS_KEY_C1D3ACB8FBF1AGK4
 sn -i OutlookTfsConnector.pfx VS_KEY_BD1B1F1C70B95C17
+sn -i OutlookTfsConnector.pfx VS_KEY_0735BCA139159F1C 
